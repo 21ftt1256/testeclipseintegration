8f4ae8defde9eee4827498c4eae5c393ba3184b0
Siti Nurqhalisah Binti Husaini <21FTT1256@student.pb.edu.bn>
2023-01-31 16:20:40 +0800

    hai

--- testeclipseintegration/src/testeclipseintegration/test/test.java
+++ testeclipseintegration/src/testeclipseintegration/test/test.java
@@ -3,7 +3,7 @@ package testeclipseintegration.test;
 public class test {
 	public static void main(String[] args) {
 		System.out.println("GitHub");
-		ClassA classA = new ClassA("ABCDEF");
+		ClassA classA = new ClassA("ABCDEF NGANTUK");
 		System.out.println(classA.getAbc()); //
 	}
 }
